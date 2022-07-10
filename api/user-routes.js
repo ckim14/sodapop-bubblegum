0b749ed93f5a0b30e26c24c9ab2cf0be46f53d5d
ckim14 <chikakim@gmail.com>
2022-07-10 12:16:40 -0400

    created routes

---
+++ api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
+
+// /api/Users
+router.route("/").get(getAllUser).post(createUser);
+
+// /api/Users/:id
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:id/friends/:friendId
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
